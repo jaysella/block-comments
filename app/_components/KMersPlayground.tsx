e4bf5480b0be1788f9f0881c6f3b900b4c1e13ea
Jay Sella <jay@jaysella.dev>
2023-08-01 22:01:18 -0400

    Add sequence, k settings to playground

--- app/_components/KMersPlayground.tsx
+++ app/_components/KMersPlayground.tsx
@@ -6,12 +6,16 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/app/_components/ui/tooltip";
-import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
-import { cn } from "@/lib/utils";
 import { useThemeDetector } from "@/app/_components/ui/use-theme-detector";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import { ArrowLeftIcon, ArrowRightIcon, Settings2Icon } from "lucide-react";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Slider } from "./ui/slider";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 function findSubstrings(sequence: string, k: number): string[] {
   const res: string[] = [];
@@ -40,12 +44,17 @@ function groupSubstrings(
   return groupedStrings;
 }
 
-export default function KMersPlayground({ sequence }: { sequence: string }) {
+export default function KMersPlayground({
+  sequence: initialSequence,
+}: {
+  sequence: string;
+}) {
   const darkMode = useThemeDetector();
   const searchParams = useSearchParams();
   const kTarget = parseInt(searchParams.get("k") || "3");
 
   const [step, setStep] = useState<number>(0);
+  const [sequence, setSequence] = useState<string>(initialSequence);
   const [k, setK] = useState<number>(
     kTarget > sequence.length ? sequence.length : kTarget
   );
@@ -62,6 +71,12 @@ export default function KMersPlayground({ sequence }: { sequence: string }) {
     },
   };
 
+  useEffect(() => {
+    if (k > sequence.length) {
+      setK(sequence.length);
+    }
+  }, [sequence, k]);
+
   return (
     <TooltipProvider>
       <div className="border-2 rounded-xl border-slate-200 dark:border-slate-800 bg-slate-50 text-slate-700 dark:text-slate-200 dark:bg-slate-900">
@@ -70,7 +85,8 @@ export default function KMersPlayground({ sequence }: { sequence: string }) {
 
           <div className="flex items-center -mr-2">
             <span className="mr-2 text-sm uppercase">
-              Step {step} of {totalSteps}
+              <span className="hidden sm:inline">Step </span>
+              {step} of {totalSteps}
             </span>
 
             <Tooltip>
@@ -98,10 +114,48 @@ export default function KMersPlayground({ sequence }: { sequence: string }) {
                 <p>Next</p>
               </TooltipContent>
             </Tooltip>
+
+            <Popover>
+              <PopoverTrigger className="p-2 rounded-lg hover:bg-slate-200 dark:hover:bg-slate-700 disabled:cursor-not-allowed disabled:hover:bg-slate-100 disabled:dark:hover:bg-slate-800">
+                <Settings2Icon size={18} />
+              </PopoverTrigger>
+              <PopoverContent align="end" className="w-full p-4 md:p-6">
+                <div className="flex flex-col gap-4">
+                  <div className="grid gap-2">
+                    <Label htmlFor="sequence">Sequence</Label>
+                    <Input
+                      id="sequence"
+                      type="text"
+                      value={sequence}
+                      onChange={(value) =>
+                        setSequence(value.target.value.toUpperCase())
+                      }
+                    />
+                  </div>
+
+                  <div className="flex items-center justify-between">
+                    <Label htmlFor="k-value">k-value</Label>
+                    <span className="w-10 rounded-md border border-transparent px-2 py-0.5 text-right text-sm hover:border-slate-200 dark:hover:border-slate-800">
+                      {k}
+                    </span>
+                  </div>
+                  <Slider
+                    id="k-value"
+                    min={1}
+                    max={sequence.length}
+                    step={1}
+                    value={[k]}
+                    onValueChange={(value) => setK(value[0])}
+                    className="[&_[role=slider]]:h-4 [&_[role=slider]]:w-4"
+                    aria-label="K Value"
+                  />
+                </div>
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
 
-        <div className="flex flex-col justify-between gap-6 px-4 py-4 divide-y-2 md:divide-y-0 md:flex-row md:divide-x-2 md:pb-6 md:px-8 divide-slate-200 dark:divide-slate-800">
+        <div className="flex flex-col justify-between gap-6 p-4 divide-y-2 md:divide-y-0 md:flex-row md:divide-x-2 md:pb-6 md:px-8 divide-slate-200 dark:divide-slate-800">
           <div className="w-max">
             <div className="-ml-2 font-mono text-5xl font-bold text-black dark:text-white">
               {sequence.split("").map((char, index) => (
@@ -130,7 +184,7 @@ export default function KMersPlayground({ sequence }: { sequence: string }) {
             </div>
           </div>
 
-          <div className="flex flex-col pt-6 gap-y-4 sm:flex-row md:divide-x-2 gap-x-6 md:pt-0 sm:justify-between">
+          <div className="flex flex-col pt-6 gap-y-4 sm:flex-row md:divide-x-2 divide-slate-200 dark:divide-slate-800 gap-x-6 md:pt-0 sm:justify-between">
             <div className="md:px-4">
               <h3 className="font-bold uppercase">{k}-mers Identified</h3>
               <ul>

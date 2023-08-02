7a847a5f8e8bc621d07f297aa311cce29ff7c4fa
Jay Sella <jay@jaysella.dev>
2023-08-02 09:15:34 -0400

    Add footer

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="container p-2 mx-auto text-xs text-center text-slate-500">
+      Designed + built by Jay Sella
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "./_components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 const spaceMono = Roboto_Mono({
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={cn(inter.className, spaceMono.variable, "dark:bg-slate-950")}
       >
         <TooltipProvider>{children}</TooltipProvider>
+        <Footer />
         <Toaster />
       </body>
     </html>

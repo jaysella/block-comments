d10e00e3a1b73521e6fcdf367b2eb451fb6f6fa5
Jay Sella <35548468+jaysella@users.noreply.github.com>
2023-08-18 22:38:03 -0400

    Improve dark link styles, link credits from footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <div className="container p-2 mx-auto my-2 text-xs text-center text-slate-500">
-      Designed + built by Jay Sella
+      Designed + built by{" "}
+      <Link className="hover:underline" href="/credits">
+        Jay Sella
+      </Link>
     </div>
   );
 }

425686b89b71d30a267e61c9bfe68e2e052c8b27
Jay Sella <35548468+jaysella@users.noreply.github.com>
2023-09-23 11:51:48 -0400

    Determine when site is loaded within an iframe

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/Footer";
+import { useEffect } from "react";
+import { toast } from "./_components/ui/use-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 const spaceMono = Roboto_Mono({
@@ -25,6 +27,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    if (window.self !== window.top) {
+      // loaded within an iframe
+      toast({ title: "Loaded within iframe" });
+    } else {
+      // not loaded within an iframe
+      console.log("I am not inside an iframe.");
+    }
+  }, []);
+
   return (
     <html lang="en">
       <body
